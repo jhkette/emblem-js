93c852c272ed5e14e1a968be71e7207ac395dfc3
khenryjoe <joseph.ketterer@gmail.com>
2019-04-17 13:35:07 +0100

    adding nav

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 window.onload = init;
 
 function init(){
+    const icon = document.getElementById("nav-icon1");
     window.addEventListener('scroll', debounce(hideNav));
+    icon.addEventListener('click', openNav);
 }
 
 // debounce to stop scroll event firing too often
@@ -23,15 +25,14 @@ function debounce(func, wait = 5, immediate = true) {
 
 
 function hideNav(){
-  
-    var w = window.innerWidth;
+
     var currentScrollPos = window.pageYOffset;
     const commonlinks = document.getElementById("commonlinkscontainer");
     const container = document.getElementById("containerlogo");
     const logo = document.getElementById("logoimage");
     const logonav = document.getElementById("logonav");
     const mainheader = document.getElementById("mainheader");
-    if((currentScrollPos > 200) && (w > 660)){
+    if(currentScrollPos > 200){
     heading.classList.add('smallheading');
     logo.classList.add("small");
     commonlinks.style.top = '-100px';
@@ -46,4 +47,9 @@ function hideNav(){
         mainheader.style.opacity = '1';  
         
     }
+}
+
+function openNav(){
+    const navigation = document.getElementById('navigation');
+    navigation.classList.toggle('openmob');
 }
