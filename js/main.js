87669fb93c1e8085deb71a70229be2337058ed68
khenryjoe <joseph.ketterer@gmail.com>
2019-04-17 11:56:23 +0100

    logo javascript added

--- js/main.js
+++ js/main.js
@@ -1,21 +1,49 @@
 window.onload = init;
 
 function init(){
-window.onscroll = function() {
-  hideNav();
+    window.addEventListener('scroll', debounce(hideNav));
 }
-}
+
+// debounce to stop scroll event firing too often
+function debounce(func, wait = 5, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
 
 
 function hideNav(){
+  
+    var w = window.innerWidth;
     var currentScrollPos = window.pageYOffset;
-    const container = document.getElementById("mainheader");
-    const logo = document.getElementById("mainlogo");
-    if(currentScrollPos > 200){
-    console.log('hello');
-    logo.style.height = "100px";
+    const commonlinks = document.getElementById("commonlinkscontainer");
+    const container = document.getElementById("containerlogo");
+    const logo = document.getElementById("logoimage");
+    const logonav = document.getElementById("logonav");
+    const mainheader = document.getElementById("mainheader");
+    if((currentScrollPos > 200) && (w > 660)){
+    heading.classList.add('smallheading');
+    logo.classList.add("small");
+    commonlinks.style.top = '-100px';
+    logonav.classList.add("nopadding");
+    mainheader.style.opacity = '.9';
     }
     else{
-        container.style.top = "0";
+        logo.classList.remove("small");
+        heading.classList.remove('smallheading');
+        logonav.classList.remove("nopadding");
+        commonlinks.style.top = '0px';
+        mainheader.style.opacity = '1';  
+        
     }
 }
