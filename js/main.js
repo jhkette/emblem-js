d489c31e4bed2480913fd8877c8da4eb4eac13fd
khenryjoe <joseph.ketterer@gmail.com>
2019-04-17 18:07:38 +0100

    more js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.onload = init;
 
-function init(){
+function init() {
     const icon = document.getElementById("nav-icon1");
     window.addEventListener('scroll', debounce(hideNav));
     icon.addEventListener('click', openNav);
@@ -9,47 +9,56 @@ function init(){
 // debounce to stop scroll event firing too often
 function debounce(func, wait = 5, immediate = true) {
     var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+    return function () {
+        var context = this,
+            args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
     };
-  };
+};
 
 
 
-function hideNav(){
-
+function hideNav() {
+    var w = window.innerWidth;
     var currentScrollPos = window.pageYOffset;
     const commonlinks = document.getElementById("commonlinkscontainer");
-    const container = document.getElementById("containerlogo");
+    
     const logo = document.getElementById("logoimage");
     const logonav = document.getElementById("logonav");
     const mainheader = document.getElementById("mainheader");
-    if(currentScrollPos > 200){
-    heading.classList.add('smallheading');
-    logo.classList.add("small");
-    commonlinks.style.top = '-100px';
-    logonav.classList.add("nopadding");
-    mainheader.style.opacity = '.9';
+   
+    if((currentScrollPos > 200) && (w > 660)){
+        heading.classList.add('smallheading');
+        logo.classList.add("small");
+        commonlinks.style.top = '-100px';
+        logonav.classList.add("nopadding");
+        mainheader.style.opacity = '.9';
+    } 
+    else if(currentScrollPos > 200){
+        commonlinks.style.top = '-100px';
+        mainheader.style.opacity = '.9';
+        logonav.classList.add("nopadding");
     }
-    else{
+    
+    else {
         logo.classList.remove("small");
         heading.classList.remove('smallheading');
         logonav.classList.remove("nopadding");
         commonlinks.style.top = '0px';
-        mainheader.style.opacity = '1';  
-        
+        mainheader.style.opacity = '1';
     }
 }
 
-function openNav(){
+function openNav() {
+    const icon = document.getElementById("nav-icon1");
     const navigation = document.getElementById('navigation');
     navigation.classList.toggle('openmob');
+    icon.classList.toggle('open');
 }
