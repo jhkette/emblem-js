e3a8f174223a24bcb1bcbb8387658c4c554878fe
khenryjoe <joseph.ketterer@gmail.com>
2019-05-08 13:20:33 +0100

    change banner.js - just used Array.from to select slide images

--- js/banner.js
+++ js/banner.js
@@ -6,53 +6,51 @@ var startBannerTimer = setInterval(function () {
 }, bannerTimer)
 
 function bannerLoop() {
-    const lead1 = document.getElementById("lead");
-    const lead2 = document.getElementById("lead1");
-    const lead3 = document.getElementById("lead2")
+    const images = Array.from(document.querySelectorAll('.leadimage'));
+    console.log(images[0]);
 
     if (bannerStatus === 1) {
-        lead2.style.opacity = 0;
+        images[1].style.opacity = 0;
         setTimeout(function () {
-            lead1.style.right = "0px";
-            lead1.style.zIndex = "1000";
-            lead2.style.right = "-100%";
-            lead2.style.zIndex = "1500";
-            lead3.style.right = "100%";
-            lead3.style.zIndex = "500";
-
+            images[0].style.right = "0px";
+            images[0].style.zIndex = "1000";
+            images[1].style.right = "-100%";
+            images[1].style.zIndex = "1500";
+            images[2].style.right = "100%";
+            images[2].style.zIndex = "500";
         }, 500)
         setTimeout(function () {
-            lead2.style.opacity = "1";
+            images[1].style.opacity = "1";
         }, 1000)
         bannerStatus = 2;
     } else if (bannerStatus === 2) {
-        lead3.style.opacity = 0;
+        images[2].style.opacity = 0;
         setTimeout(function () {
-            lead2.style.right = "0px";
-            lead2.style.zIndex = "1000";
-            lead3.style.right = "-100%";
-            lead3.style.zIndex = "1500";
-            lead1.style.right = "100%";
-            lead1.style.zIndex = "500";
+            images[1].style.right = "0px";
+            images[1].style.zIndex = "1000";
+            images[2].style.right = "-100%";
+            images[2].style.zIndex = "1500";
+            images[0].style.right = "100%";
+            images[0].style.zIndex = "500";
 
         }, 500)
         setTimeout(function () {
-            lead3.style.opacity = "1";
+            images[2].style.opacity = "1";
         }, 1000)
         bannerStatus = 3;
     } else if (bannerStatus === 3) {
-        lead1.style.opacity = 0;
+        images[0].style.opacity = 0;
         setTimeout(function () {
-            lead3.style.right = "0px";
-            lead3.style.zIndex = "1000";
-            lead1.style.right = "-100%";
-            lead1.style.zIndex = "1500";
-            lead2.style.right = "100%";
-            lead2.style.zIndex = "500";
+            images[2].style.right = "0px";
+            images[2].style.zIndex = "1000";
+            images[0].style.right = "-100%";
+            images[0].style.zIndex = "1500";
+            images[1].style.right = "100%";
+            images[1].style.zIndex = "500";
 
         }, 500)
         setTimeout(function () {
-            lead1.style.opacity = "1";
+            images[0].style.opacity = "1";
         }, 1000)
         bannerStatus = 1;
     }
